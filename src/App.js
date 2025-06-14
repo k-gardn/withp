35307495fdbe057381062a79a1afe69ce815a7d2
unknown <choeimk@gmail.com>
2025-06-14 16:43:48 +0900

    민경테스트

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          할수있다!!!! <code>src/App.js</code> and save to reload.
+          할수있다mk!!!! <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
